20a8c9179692841d09fd5f1845d006442c5d01d9
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-03-20 10:49:00 +0000

    work still in progress animation still not right

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -61,41 +61,119 @@ const WorkPage = () => {
   }, [])
 
   return (
+    // <main className="w-full h-full flex flex-col relative">
+    //   {slideInfo.map((slide, index) => (
+    //     <section
+    //       key={Date.now() * Math.random()}
+    //       className={`w-full absolute flex flex-col lg:flex-row transition-all duration-[4000ms] ease-in-out ${currentSlide === index ? "translate-y-0 h-full opacity-100" : "-translate-y-full opacity-0"} `} >
+    //       <figure className="w-full h-9/12 lg:w-9/12">
+    //         <img className="w-full h-full object-contain bg-gray-200" src={slide.image} alt={slide.imageAlt} />
+    //       </figure>
+    //       <aside className="w-full flex flex-col flex-1 justify-between bg-gray-700 text-amber-50">
+    //         <div className="p-4 lg:h-10/12 lg:flex lg:flex-col lg:justify-center ">
+    //           <h2 className=" my-2 text-lg lg:text-xl lg:border-b lg:mb-8 lg:pb-4">{slide.h2}</h2>
+    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p}</p>
+    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p1}
+    //             <Link href={slide.link1URL}
+    //               target="_blank"
+    //               className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link1Text}</Link></p>
+    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p2}
+    //             <Link href={slide.link2URL}
+    //               target="_blank"
+    //               className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link2Text}</Link></p>
+    //         </div>
+    //         <div className="flex pl-4 lg:h-1/10">
+    //           <button className="cursor-pointer hover:text-gray-300"
+    //             onClick={(e) => handleNext(e)}>
+    //             <ChevronDown size={42} />
+    //           </button>
+    //           <button
+    //             className={`cursor-pointer hover:text-gray-300 ${currentSlide === 0 ? 'hidden' : ''}`}
+
+    //             onClick={(e) => handlePrev(e)}
+    //           >
+    //             <ChevronUp size={42} />
+    //           </button>
+    //         </div>
+    //       </aside>
+    //     </section>
+    //   ))}
+    // </main>
+
+
     <main className="w-full h-full flex flex-col relative">
-      {slideInfo.map((slide, index) => (
-        <section
-          key={Date.now() * Math.random()}
-          className={`w-full absolute ${currentSlide === index ? "top-0 bottom-0 visible" : "-translate-y-200 opacity-0"} flex flex-col lg:flex-row transition-all duration-700`} >
-          <figure className="w-full h-9/12 lg:w-9/12">
-            <img className="w-full h-full object-contain bg-gray-200" src={slide.image} alt={slide.imageAlt} />
-          </figure>
-          <aside className="w-full flex flex-col flex-1 justify-between bg-gray-700 text-amber-50">
-            <div className="p-4 lg:h-10/12 lg:flex lg:flex-col lg:justify-center ">
-              <h2 className=" my-2 text-lg lg:text-xl lg:border-b lg:mb-8 lg:pb-4">{slide.h2}</h2>
-              <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p}</p>
-              <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p1}
-                <Link href={slide.link1URL}
-                  target="_blank"
-                  className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link1Text}</Link></p>
-              <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p2}
-                <Link href={slide.link2URL}
-                  target="_blank"
-                  className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link2Text}</Link></p>
-            </div>
-            <div className="flex pl-4 lg:h-1/10">
-              <button className={`cursor-pointer hover:text-gray-300 ${currentSlide === 3 ? 'hidden' : ''}`}
-                onClick={(e) => handleNext(e)}>
-                <ChevronDown size={42} />
-              </button>
-              <button className="cursor-pointer hover:text-gray-300"
-                onClick={(e) => handlePrev(e)}
-              >
-                <ChevronUp size={42} />
-              </button>
-            </div>
-          </aside>
-        </section>
-      ))}
+      <section
+        className={`w-full absolute flex flex-col lg:flex-row transition-all duration-[3000ms] ease-in-out  h-full opacity-100 ${currentSlide === 0 ? "" : "-translate-y-full opacity-0"} `} >
+        <figure className="w-full h-9/12 lg:w-9/12">
+          <img className="w-full h-full object-contain bg-gray-200" src={slideInfo[0].image} alt={slideInfo[0].imageAlt} />
+        </figure>
+        <aside className="w-full flex flex-col flex-1 justify-between bg-gray-700 text-amber-50">
+          <div className="p-4 lg:h-10/12 lg:flex lg:flex-col lg:justify-center ">
+            <h2 className=" my-2 text-lg lg:text-xl lg:border-b lg:mb-8 lg:pb-4">{slideInfo[0].h2}</h2>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[0].p}</p>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[0].p1}
+              <Link href={slideInfo[0].link1URL}
+                target="_blank"
+                className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slideInfo[0].link1Text}</Link></p>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[0].p2}
+              <Link href={slideInfo[0].link2URL}
+                target="_blank"
+                className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slideInfo[0].link2Text}</Link></p>
+          </div>
+          <div className="flex pl-4 lg:h-1/10">
+            <button className="cursor-pointer hover:text-gray-300"
+              onClick={(e) => handleNext(e)}>
+              <ChevronDown size={42} />
+            </button>
+            <button
+              className={`cursor-pointer hover:text-gray-300 ${currentSlide === 0 ? 'hidden' : ''}`}
+
+              onClick={(e) => handlePrev(e)}
+            >
+              <ChevronUp size={42} />
+            </button>
+          </div>
+        </aside>
+      </section>
+      {/* SECOND SLIDE */}
+
+      <section
+        className={`w-full flex flex-col lg:flex-row transition-all duration-[3000ms] ease-in-out h-full  ${currentSlide === 1 ? "" : "-translate-y-full opacity-0"} `} >
+        <figure
+          className="w-full h-9/12 lg:w-9/12">
+          <img className="w-full h-full object-contain bg-gray-200" src={slideInfo[1].image} alt={slideInfo[1].imageAlt} />
+        </figure>
+        <aside className="w-full flex flex-col flex-1 justify-between bg-gray-700 text-amber-50">
+          <div className="p-4 lg:h-10/12 lg:flex lg:flex-col lg:justify-center ">
+            <h2 className=" my-2 text-lg lg:text-xl lg:border-b lg:mb-8 lg:pb-4">{slideInfo[1].h2}</h2>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[1].p}</p>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[1].p1}
+              <Link href={slideInfo[1].link1URL}
+                target="_blank"
+                className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slideInfo[1].link1Text}</Link></p>
+            <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slideInfo[1].p2}
+              <Link href={slideInfo[1].link2URL}
+                target="_blank"
+                className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slideInfo[1].link2Text}</Link></p>
+          </div>
+          <div className="flex pl-4 lg:h-1/10">
+            <button className="cursor-pointer hover:text-gray-300"
+              onClick={(e) => handleNext(e)}>
+              <ChevronDown size={42} />
+            </button>
+            <button
+              className={`cursor-pointer hover:text-gray-300 ${currentSlide === 0 ? 'hidden' : ''}`}
+
+              onClick={(e) => handlePrev(e)}
+            >
+              <ChevronUp size={42} />
+            </button>
+          </div>
+        </aside>
+      </section>
+
+
+
     </main>
   )
 }
