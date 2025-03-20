ecdfac12c4ae33ef299cdd5715072c03e2454b17
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-03-20 13:58:59 +0000

    moved slideInfo in utils

--- app/utils/helper.ts
+++ app/utils/helper.ts
@@ -7,3 +7,42 @@ export function saltAndHashPassword(password: any) {
   const hash = bcrypt.hashSync(password, salt)
   return hash;
 }
+
+export const slideInfo = [
+  {
+    image: '/hedgehog.png',
+    imageAlt: "Hedgehog landing page",
+    h2: 'Hedgehog Finances App',
+    p: `Real time stock tracking app built with Next.js and Tailwind, using MongDB,Prisma ORM , Next-auth and cron-job.org. This project was developed with 4 other colleagues as part of our graduation project for Northcoders.com Javascript Bootcamp. `,
+    p1: 'Check out the live app here:',
+    link1URL: 'https://hedgehog-finances-nine.vercel.app/',
+    link1Text: 'Hedgehog App',
+    p2: 'See the Github repo here:',
+    link2URL: 'https://github.com/patrickfoulger1/hedgehog-finances',
+    link2Text: 'Hedgehog Repo'
+  },
+  {
+    image: '/nnc-news.png',
+    imageAlt: 'NNC News dashboard page',
+    h2: 'NNC News',
+    p: 'Articles platform app developed using NodeJS, ExpressJS for backend and React on the front end.',
+    p1: 'See live version here:',
+    link1URL: 'https://nnc-andi.netlify.app/',
+    link1Text: 'NNC News',
+    p2: 'See the Github repo here:',
+    link2URL: "https://github.com/aeriallistique/nc-news-react?tab=readme-ov-file",
+    link2Text: 'Github repo'
+  },
+  {
+    image: '/pull-up-timer.png',
+    imageAlt: 'pull-up timer on a pink background',
+    h2: 'Pull-up Timer',
+    p: 'A small app I developed to help me train and beat the World Record for Most Pull-ups in 24 hours.',
+    p1: 'See live version here:',
+    link1URL: 'https://wr-pull-up-timer.netlify.app/',
+    link1Text: 'Pull-up Timer',
+    p2: 'See the Github repo here:',
+    link2URL: "https://github.com/aeriallistique/pullup-timer-counter",
+    link2Text: 'Github repo'
+  },
+]
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,45 +2,8 @@
 import { ChevronUp, ChevronDown } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
+import { slideInfo } from "../utils/helper"
 
-const slideInfo = [
-  {
-    image: '/hedgehog.png',
-    imageAlt: "Hedgehog landing page",
-    h2: 'Hedgehog Finances App',
-    p: 'Real time stock tracking app built with Next.js and Tailwind, using MongDB,Prisma ORM , Next-auth and cron-job.org',
-    p1: 'Check out the live app here:',
-    link1URL: 'https://hedgehog-finances-nine.vercel.app/',
-    link1Text: 'Hedgehog App',
-    p2: 'See the Github repo here:',
-    link2URL: 'https://github.com/patrickfoulger1/hedgehog-finances',
-    link2Text: 'Hedgehog Repo'
-  },
-  {
-    image: '/nnc-news.png',
-    imageAlt: 'NNC News dashboard page',
-    h2: 'NNC News',
-    p: 'Articles platform app developed using NodeJS, ExpressJS for backend and React on the front end.',
-    p1: 'See live version here:',
-    link1URL: 'https://nnc-andi.netlify.app/',
-    link1Text: 'NNC News',
-    p2: 'See the Github repo here:',
-    link2URL: "https://github.com/aeriallistique/nc-news-react?tab=readme-ov-file",
-    link2Text: 'Github repo'
-  },
-  {
-    image: '/pull-up-timer.png',
-    imageAlt: 'pull-up timer on a pink background',
-    h2: 'Pull-up Timer',
-    p: 'A small app I developed to help me train and beat the World Record for Most Pull-ups in 24 hours.',
-    p1: 'See live version here:',
-    link1URL: 'https://wr-pull-up-timer.netlify.app/',
-    link1Text: 'Pull-up Timer',
-    p2: 'See the Github repo here:',
-    link2URL: "https://github.com/aeriallistique/pullup-timer-counter",
-    link2Text: 'Github repo'
-  },
-]
 
 
 const WorkPage = () => {
@@ -48,7 +11,7 @@ const WorkPage = () => {
   const isScrolling = useRef(false)
   const handleNext = (e: React.MouseEvent<HTMLElement> | WheelEvent) => {
     e.preventDefault()
-    setCurrentSlide((prev) => prev < 3 ? prev + 1 : prev)
+    setCurrentSlide((prev) => prev < slideInfo.length - 1 ? prev + 1 : prev)
   }
   const handlePrev = (e: React.MouseEvent<HTMLElement> | WheelEvent) => {
     e.preventDefault()
@@ -73,46 +36,6 @@ const WorkPage = () => {
   }, [])
 
   return (
-    // <main className="w-full h-full flex flex-col relative">
-    //   {slideInfo.map((slide, index) => (
-    //     <section
-    //       key={Date.now() * Math.random()}
-    //       className={`w-full absolute flex flex-col lg:flex-row transition-all duration-[4000ms] ease-in-out ${currentSlide === index ? "translate-y-0 h-full opacity-100" : "-translate-y-full opacity-0"} `} >
-    //       <figure className="w-full h-9/12 lg:w-9/12">
-    //         <img className="w-full h-full object-contain bg-gray-200" src={slide.image} alt={slide.imageAlt} />
-    //       </figure>
-    //       <aside className="w-full flex flex-col flex-1 justify-between bg-gray-700 text-amber-50">
-    //         <div className="p-4 lg:h-10/12 lg:flex lg:flex-col lg:justify-center ">
-    //           <h2 className=" my-2 text-lg lg:text-xl lg:border-b lg:mb-8 lg:pb-4">{slide.h2}</h2>
-    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p}</p>
-    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p1}
-    //             <Link href={slide.link1URL}
-    //               target="_blank"
-    //               className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link1Text}</Link></p>
-    //           <p className="text-sm font-extralight mb-1 lg:border-b lg:mb-8 lg:pb-4">{slide.p2}
-    //             <Link href={slide.link2URL}
-    //               target="_blank"
-    //               className="ml-2 font-medium underline hover:text-blue-400" id="lab_exp">{slide.link2Text}</Link></p>
-    //         </div>
-    //         <div className="flex pl-4 lg:h-1/10">
-    //           <button className="cursor-pointer hover:text-gray-300"
-    //             onClick={(e) => handleNext(e)}>
-    //             <ChevronDown size={42} />
-    //           </button>
-    //           <button
-    //             className={`cursor-pointer hover:text-gray-300 ${currentSlide === 0 ? 'hidden' : ''}`}
-
-    //             onClick={(e) => handlePrev(e)}
-    //           >
-    //             <ChevronUp size={42} />
-    //           </button>
-    //         </div>
-    //       </aside>
-    //     </section>
-    //   ))}
-    // </main>
-
-
     <main className="w-full h-full flex flex-col relative">
       <section
         className={`w-full h-full absolute flex flex-col lg:flex-row transition-all duration-1000 ease-in-out  opacity-100 ${currentSlide === 0 ? "" : "-translate-y-full opacity-0"} `} >
