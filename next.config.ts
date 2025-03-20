8803a828e341df2d6055c4147afd5d0ecb0cc6a4
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-03-20 09:33:52 +0000

    removed devIndicatior

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false,
   images: {
     domains: ['avatars.githubusercontent.com', "*.googleusercontent.com", 'lh3.googleusercontent.com'],
     remotePatterns: [
